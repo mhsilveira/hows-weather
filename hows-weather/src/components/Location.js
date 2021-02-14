1ffaea304f971496b532e35ba90fddaf529ccf56
Matheus Silveira <blanchimaah@gmail.com>
2021-02-14 20:25:32 -0300

    Alteracoes

--- hows-weather/src/components/Location.js
+++ hows-weather/src/components/Location.js
@@ -1,9 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import {Alerta, WeatherCard} from '../style/global';
-
 // REACT_APP_OPEN_WEATHER_API_KEY=bb1f21f8f4a1d0d8c6caf10c728aaa4f
-
 
 const Location = () => {
   const [location, setLocation] = useState(false);
@@ -22,17 +20,19 @@ const Location = () => {
           format: 'json'
         }
       }).then(res => {
-        // var xxx = res.data.json();
-        setWeather(res.data);
+        // futuramente, pegar apenas da data que o usuario selecionar e passar ali pro includes
+        const dailyData = res.data.list.filter(dailyForecast => dailyForecast.dt_txt.includes("18:00:00"));
+        setWeather(dailyData);
       })
     } catch (error) {
       console.error(error);
+      setError(true);
       if(error.response.status === 401){
         setError(error.response.statusText);
       }
     }
   }
-  
+
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       setLocation(true);
@@ -52,13 +52,12 @@ const Location = () => {
         <h3>É necessário habilitar a localização no browser para obter dados da API.</h3>
       </Alerta>
     }      
-    { weather && weather.list.map(item => (
+    { weather && weather.map(item => (
       console.log(item),
       <WeatherCard key={item.dt}>
         <div className="temperature">{item.main.temp}</div>
         <div className="temp-min">{item.main.temp_min}</div>
         <div className="temp-max">{item.main.temp_max}</div>
-        {item.dt}
       </WeatherCard>
     ))
     }
